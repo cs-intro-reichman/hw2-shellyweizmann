3ef422b1c16df2d82304583c88e739d8ebbd3549
Shelly Weizmann <shellyweizmann@Shellys-MacBook-Pro.local>
2025-11-12 17:15:36 +0200

    Completed assignment 2

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,39 @@ Great work boaz!
 
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+ public static void main(String[] args) 
+ {
+        
+        String wordString = args[0];          
+        int cheerCount = Integer.parseInt(args[1]);  
+
+        String specialLetters = "AEFHILMNORSX"; 
+
+        for (int i = 0; i < wordString.length(); i++) 
+        {
+            char ch = wordString.charAt(i); 
+
+            String connector; 
+            if (specialLetters.indexOf(ch) != -1)
+            {  
+                connector = "an ";  
+            }
+             else 
+            {
+                connector = "a  ";  
+            }
+
+            
+            System.out.println("Give me " + connector + ch + "!");
         }
+
+        System.out.println("What does that spell?");
+
+        for (int i = 0; i < cheerCount; i++) 
+        {
+            System.out.println(wordString + "!!!");
+        }
+    
 }
+}        
+
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,52 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+	public static void main(String args[])
+	 {
+	    int N = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int seed = 1;
+	if(mode.equals("v")){
+		System.out.print(seed + " ");
+		int sum = 1;
+		do{
+			if(seed % 2 == 0){
+				seed = seed / 2;
+				sum++;
+			} else{
+				seed = seed * 3 + 1;
+				sum++;
+			}
+			System.out.print(seed + " ");
+		}while(seed != 1); 
+		System.out.print("(" + sum + ")");
+		
+		sum = 1;
+		System.out.println();
+		for (int i = 1; i < N; i++){
+			seed = i+1;
+
+			System.out.print(seed + " ");
+			while(seed != 1){
+			if(seed % 2 == 0){
+				seed = seed / 2;
+				sum++;
+			} else{
+				seed = seed * 3 + 1;
+				sum++;
+			}
+		
+			System.out.print(seed + " ");
+		}
+		
+		System.out.print("(" + sum + ")");
+		System.out.println();
+		sum = 1;
+
 	}
+		System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+} else{
+	System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+}
+
+}
 }
